0d6e897a1c1a0ba80c61e34433cda3ed0d599809
rogengell <niel4921@easv365.dk>
2025-01-05 13:47:36 +0100

    Update

--- Lisener/obj/Debug/net8.0/Lisener.AssemblyInfo.cs
+++ Lisener/obj/Debug/net8.0/Lisener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lisener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764597139d936b51e2cf0fec1ce4f5803a1c721f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2e39064bae605d7b3de6ba7afdabde1317a723")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lisener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lisener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
